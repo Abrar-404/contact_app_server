e05a481b8aa3e2bf3a25a0033a8fae9090b22176
Sahil Abrar <zeroxkhan2@gmail.com>
2024-01-05 20:02:25 +0600

    added delete function

--- index.js
+++ index.js
@@ -74,9 +74,15 @@ async function run() {
         },
       };
       const result = await contactCollection.updateOne(filter, updatedDoc);
-      console.log('Update Result:', result);
       res.send(result);
     });
+
+     app.delete('/addContact/:id', async (req, res) => {
+       const id = req.params.id;
+       const query = { _id: new ObjectId(id) };
+       const result = await contactCollection.deleteOne(query);
+       res.send(result);
+     });
 
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 });
